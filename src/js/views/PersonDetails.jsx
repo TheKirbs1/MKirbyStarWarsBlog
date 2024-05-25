ad8f561b66add8cdd176cbb2d243da2a85378dc4
Matt Kirby <160439337+TheKirbs1@users.noreply.github.com>
2024-05-25 04:24:12 +0000

    styling added to PersonDetails

--- src/js/views/PersonDetails.jsx
+++ src/js/views/PersonDetails.jsx
@@ -14,39 +14,16 @@ const PersonDetails = () => {
 
 	return (
 
-		
-// 		<div className="jumbotron text-white">
-// 			<h1 className="display-4">{store.personDetails.properties.name}</h1>
-// 			<h1 className="display-4">{store.personDetails.properties.gender}</h1>
-
-// 			<hr className="bg-danger border-2 border-top border-danger" />
-
-// 			<Link to="/">
-// 				<span className="btn btn-primary btn-lg" href="#" role="button">
-// 					Back home
-// 				</span>
-// 			</Link>
-// 		</div>
-// 	);
-// };
-
-
-// export default PersonDetails;
-
-// PersonDetails.propTypes = {
-// 	match: PropTypes.object
-// };
-
  <>
-			<div className="card mb-3">
+			<div className="card mb-3 border-0 bg-dark text-white">
 			<div className="row g-0">
 				<div className="col-md-4">
-					<img src="" className="img-fluid rounded-start" alt="..." />
+					<img src="" className="img-fluid rounded-start mx-3" alt="..." />
 				</div>
 				<div className="col-md-8">
-					<div className="card-body">
-						<h2 className="card-title">{store.personDetails.properties.name}</h2>
-						<p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					<div className="card-body container-fluid mx-5 px-5">
+						<h1 className="card-title h-100 d-flex align-items-center justify-content-center">{store.personDetails.properties.name}</h1>
+						<p className="card-text h-100 mx-5 px-5 py-2 d-flex align-items-center justify-content-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc sed blandit libero volutpat sed cras ornare. Sit amet consectetur adipiscing elit duis tristique sollicitudin nibh. Morbi non arcu risus quis varius quam. Massa placerat duis ultricies lacus sed turpis tincidunt id aliquet. Morbi enim nunc faucibus a pellentesque sit amet porttitor eget. Viverra mauris in aliquam sem fringilla ut. Neque ornare aenean euismod elementum. Sapien pellentesque habitant morbi tristique. Ut consequat semper viverra nam libero justo laoreet sit. Arcu dui vivamus arcu felis bibendum ut. Suspendisse sed nisi lacus sed viverra. Imperdiet dui accumsan sit amet nulla facilisi. Eget mauris pharetra et ultrices neque ornare aenean euismod elementum.</p>
 					</div>
 				</div>
 				<div className="col-md-4"></div>
@@ -54,19 +31,23 @@ const PersonDetails = () => {
 			</div>
 			<div>
 			<hr className="bg-danger border-2 border-top border-danger" />
-			<ul className="list-group list-group-horizontal">
-			<li className="list-group-item">Name <br />{store.personDetails.properties.name}</li>
-			<li className="list-group-item">Birth Year <br />{store.personDetails.properties.birth_year}</li>
-			<li className="list-group-item">Gender <br />{store.personDetails.properties.gender}</li>
-			<li className="list-group-item">Height <br />{store.personDetails.properties.height}</li>
-			<li className="list-group-item">Skin Color <br />{store.personDetails.properties.skin_color}</li>
-			<li className="list-group-item">Eye Color <br />{store.personDetails.properties.eye_color}</li>
-			</ul>
-						<Link to="/">
+			<div className="container-fluid d-flex align-items-center justify-content-center">
+					<ul className="list-group list-group-horizontal bg-dark">
+					<li className="list-group-item bg-dark text-info border-1 border-light mx-3 py-3 text-center">Name <br />{store.personDetails.properties.name}</li>
+					<li className="list-group-item bg-dark text-warning border-1 border-light mx-3 py-3 text-center">Birth Year <br />{store.personDetails.properties.birth_year}</li>
+					<li className="list-group-item bg-dark text-info border-1 border-light mx-3 py-3 text-center">Gender <br />{store.personDetails.properties.gender}</li>
+					<li className="list-group-item bg-dark text-warning border-1 border-light mx-3 py-3 text-center">Height <br />{store.personDetails.properties.height}</li>
+					<li className="list-group-item bg-dark text-info border-1 border-light mx-3 py-3 text-center">Skin Color <br />{store.personDetails.properties.skin_color}</li>
+					<li className="list-group-item bg-dark text-warning border-1 border-light mx-3 py-3 text-center">Hair Color <br />{store.personDetails.properties.hair_color}</li>
+					<li className="list-group-item bg-dark text-info border-1 border-light mx-3 py-3 text-center">Mass<br />{store.personDetails.properties.mass}</li>
+					<li className="list-group-item bg-dark text-warning border-1 border-light mx-3 py-3 text-center">Eye Color <br />{store.personDetails.properties.eye_color}</li>
+				</ul>
+			</div>
+						{/* <Link to="/">
 							<span className="btn btn-primary btn-lg" href="#" role="button">
 								Back home
 							</span>
-						</Link>
+						</Link> */}
 			</div>
 </>
 	);
